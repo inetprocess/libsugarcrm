ad375700dad5fe714f7cf753b515886c9466c51e
Rémi Sauvat <remi.sauvat@inetprocess.com>
2015-08-31 13:19:08 +0200

    Update doc and author.

--- src/InstallerException.php
+++ src/InstallerException.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * SugarCRM Tools
+ *
+ * PHP Version 5.3 -> 5.6
+ * SugarCRM Versions 6.5 - 7.6
+ *
+ * @author Rémi Sauvat
+ * @copyright 2005-2015 iNet Process
+ * @package inetprocess/sugarcrm
+ * @license GNU General Public License v2.0
+ * @link http://www.inetprocess.com
+ */
 namespace Inet\SugarCRM;
 
 class InstallerException extends \RuntimeException
