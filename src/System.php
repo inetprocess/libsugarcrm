8a27a18a1d1f19c10da63e861e42baa6b26b065d
Rémi Sauvat <remi.sauvat@inetprocess.com>
2016-04-01 12:29:13 +0200

    Add more granular repair and rebuild.

--- src/System.php
+++ src/System.php
@@ -45,6 +45,12 @@ class System
     protected $messages = array();
 
     /**
+     * Need to flush ob before getting messages ?
+     * @var    boolean
+     */
+    protected $isFlushed = true;
+
+    /**
      * Set the LogPrefix to be unique and ask for an Entry Point to SugarCRM
      *
      * @param EntryPoint $entryPoint Enters the SugarCRM Folder
@@ -66,12 +72,11 @@ class System
     }
 
     /**
-     * Add a message to the array
-     * @param    string    $message
+     * @deprecated Use repairAll instead
      */
-    public function addMessage($message)
+    public function repair($executeSql = false, $userId = '1')
     {
-        $this->messages[] = $message;
+        return $this->repairAll($executeSql, $userId);
     }
 
     /**
@@ -81,46 +86,97 @@ class System
      * @param     string     $user_id       User id of the admin user
      * @return    array                     Messages
      */
-    public function repair($executeSql = false, $user_id = '1')
+    public function repairAll($executeSql = false, $userId = '1')
     {
-        // Config ang language
-        $sugarConfig = $this->getEntryPoint()->getApplication()->getSugarConfig();
-        $this->getEntryPoint()->setCurrentUser($user_id);
-        $currentLanguage = $sugarConfig['default_language'];
-
-        // check that I can repair (old sugar)
-        $qrrFile = 'modules/Administration/QuickRepairAndRebuild.php';
-        if (!file_exists($qrrFile)) {
-            throw new \RuntimeException("Can't load the QuickRepairAndRebuild class from SugarCRM.");
-        }
-        require_once($qrrFile);
-
-        $self = $this;
-        ob_start(function ($message) use ($self) {
-            $message = preg_replace('#<script.*</script>#i', '', $message);
-            $message = preg_replace('#<(br\s*/?|/h3)>#i', PHP_EOL, $message);
-            $message = trim(strip_tags($message));
-            $message = preg_replace('#'.PHP_EOL.'{2,}#', PHP_EOL, $message);
-            $self->addMessage(trim($message));
-            return '';
-        });
-
-        // Repair and catch the output
-        require_once('include/utils/layout_utils.php');
-        $GLOBALS['mod_strings'] = return_module_language($currentLanguage, 'Administration');
+        $this->setUpQuickRepair($userId);
         $repair = new \RepairAndClear();
         $repair->repairAndClearAll(array('clearAll'), array(translate('LBL_ALL_MODULES')), $executeSql, true, '');
-        ob_end_flush();
-
         //remove the js language files
         if (!method_exists('LanguageManager', 'removeJSLanguageFiles')) {
             $this->getLogger()->warning('No removeJSLanguageFiles method (sugar too old?). Check that it\'s clean.');
         } else {
             \LanguageManager::removeJSLanguageFiles();
         }
+        return $this->getMessages();
+    }
 
-        $this->tearDown();
+    /**
+     * Rebuild only Extensions.
+     * @param     array      $modules       Rebuild only the specified modules
+     * @param     string     $user_id       User id of the admin user
+     * @return    array                     Messages
+     */
+    public function rebuildExtensions(array $modules = array(), $user_id = '1')
+    {
+        $this->setUpQuickRepair($user_id);
+        $repair = new \RepairAndClear();
+        $repair->repairAndClearAll(
+            array('rebuildExtensions'),
+            $modules
+        );
+        return $this->getMessages();
+    }
 
+    /**
+     * Specific for a fast rebuild of Extentions files for application
+     */
+    public function rebuildApplication()
+    {
+        require_once('ModuleInstall/ModuleInstaller.php');
+        $moduleInstaller = new \ModuleInstaller();
+        $moduleInstaller->silent = true;
+        $moduleInstaller->rebuild_all(true, array('application'));
+    }
+
+    /**
+     * Prepare all the necessary thing to run a repair and rebuild
+     * ob is also started to catch any output
+     */
+    private function setUpQuickRepair($user_id = '1')
+    {
+        // Config ang language
+        $sugarConfig = $this->getEntryPoint()->getApplication()->getSugarConfig();
+        // Force setting the admin user.
+        $this->getEntryPoint()->setCurrentUser($user_id);
+        $currentLanguage = $sugarConfig['default_language'];
+        require_once('modules/Administration/QuickRepairAndRebuild.php');
+        require_once('include/utils/layout_utils.php');
+        $GLOBALS['mod_strings'] = return_module_language($currentLanguage, 'Administration');
+        ob_start(array($this, 'parseOutput'));
+        $this->isFlushed = false;
+    }
+
+    /**
+     * Parse ouput of ob to remove html
+     * and store messages.
+     */
+    private function parseOutput($message)
+    {
+        $message = preg_replace('#<script.*</script>#i', '', $message);
+        $message = preg_replace('#<(br\s*/?|/h3)>#i', PHP_EOL, $message);
+        $message = trim(strip_tags($message));
+        $message = preg_replace('#'.PHP_EOL.'{2,}#', PHP_EOL, $message);
+        $this->addMessage(trim($message));
+        return '';
+    }
+
+    /**
+     * Add a message to the array
+     * @param    string    $message
+     */
+    private function addMessage($message)
+    {
+        $this->messages[] = $message;
+    }
+
+    /**
+     * Flush ob if necessary and return stored messages.
+     */
+    public function getMessages()
+    {
+        if ($this->isFlushed !== true) {
+            ob_end_flush();
+        }
         return $this->messages;
     }
 
@@ -139,18 +195,5 @@ class System
     public function isActivityEnabled()
     {
         return \Activity::isEnabled();
-    }
-
-    /**
-     * Taken from fayebsg/sugarcrm-cli
-     * Useful to clean Sugar before leaving it
-     */
-    protected function tearDown()
-    {
-        sugar_cleanup(false);
-        if (class_exists('DBManagerFactory')) {
-            $db = \DBManagerFactory::getInstance();
-            $db->disconnect();
-        }
     }
 }
--- tests/SystemTest.php
+++ tests/SystemTest.php
@@ -26,16 +26,61 @@ class SystemTest extends SugarTestCase
     }
 
     /**
+     * This test is really slow ( > 1 minute)
      * @group sugarcrm-slow
+     * @group repair
      */
-    public function testRepair()
+    public function testRepairAll()
     {
         $checkFile = realpath(getenv('SUGARCRM_PATH') . '/cache/class_map.php');
         $this->assertFileExists($checkFile, 'That file is used to test my repair');
         unlink($checkFile);
         $this->assertFileNotExists($checkFile, 'That file is used to test my repair');
         $system = new System($this->getEntryPointInstance());
-        $system->repair();
+        $ret = $system->repair();
         $this->assertFileExists($checkFile, 'That file is used to test my repair');
+        $this->assertNotEmpty($ret);
+        $this->assertNotEmpty($ret[0]);
+    }
+
+    /**
+     * @group repair
+     */
+    public function testRebuildExtensions()
+    {
+        $sugar_path = realpath(getenv('SUGARCRM_PATH'));
+        $ext_path = $sugar_path . '/custom/Extension/modules/Accounts/Ext/LogicHooks/test_rebuild_ext.php';
+        $compiled_ext_path = $sugar_path . '/custom/modules/Accounts/Ext/LogicHooks/logichooks.ext.php';
+        $token = md5(mt_rand());
+        if (!is_dir(dirname($ext_path))) {
+            mkdir(dirname($ext_path), 0750, true);
+        }
+        file_put_contents($ext_path, "<?php\n// token: $token");
+        $sys = new System($this->getEntryPointInstance());
+        $ret = $sys->rebuildExtensions(array('Accounts'));
+        $this->assertCount(2, $ret);
+        $this->assertFileExists($compiled_ext_path);
+        $compiled_ext = file_get_contents($compiled_ext_path);
+        $this->assertContains("// token: $token", $compiled_ext);
+    }
+
+    /**
+     * @group repair
+     */
+    public function testRebuildApplication()
+    {
+        $sugar_path = realpath(getenv('SUGARCRM_PATH'));
+        $ext_path = $sugar_path . '/custom/Extension/application/Ext/Utils/test_rebuild_application.php';
+        $compiled_ext_path = $sugar_path . '/custom/application/Ext/Utils/custom_utils.ext.php';
+        $token = md5(mt_rand());
+        if (!is_dir(dirname($ext_path))) {
+            mkdir(dirname($ext_path), 0750, true);
+        }
+        file_put_contents($ext_path, "<?php\n// token: $token");
+        $sys = new System($this->getEntryPointInstance());
+        $sys->rebuildApplication();
+        $this->assertFileExists($compiled_ext_path);
+        $compiled_ext = file_get_contents($compiled_ext_path);
+        $this->assertContains("// token: $token", $compiled_ext);
     }
 }
