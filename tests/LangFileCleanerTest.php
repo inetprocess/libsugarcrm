fdaed3d13336f7e24d96db8d61506ad2e9478716
Rémi Sauvat <remi.sauvat@inetprocess.com>
2016-04-01 13:31:39 +0200

    Test LangFileCleaner with 100% coverage.

--- tests/LangFileCleanerTest.php
+++ tests/LangFileCleanerTest.php
@@ -10,6 +10,20 @@ use Inet\SugarCRM\Tests\TestsUtil\TestLogger;
 
 class LangFileCleanerTest extends \PHPUnit_Framework_TestCase
 {
+    public static $invalid_file = '';
+
+    public static function setUpBeforeClass()
+    {
+        self::$invalid_file = __DIR__ . '/fake_sugar/custom/include/language/invalid.lang.php';
+    }
+
+    public static function tearDownAfterClass()
+    {
+        if (file_exists(self::$invalid_file)) {
+            unlink(self::$invalid_file);
+        }
+    }
+
     public function testCleanEmpty()
     {
         $logger = new TestLogger();
@@ -24,5 +38,23 @@ class LangFileCleanerTest extends \PHPUnit_Framework_TestCase
         $logger = new TestLogger();
         $cleaner = new LangFileCleaner(new Application($logger, $fake_sugar));
         $this->assertTrue($cleaner->clean());
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessageRegexp /Unable to load the file contents of/
+     */
+    public function testCleanFailure()
+    {
+        \PHPUnit_Framework_Error_Warning::$enabled = false;
+        $err_level = error_reporting();
+        error_reporting($err_level &~ E_WARNING);
+        $fake_sugar = __DIR__ . '/fake_sugar';
+        touch(self::$invalid_file);
+        chmod(self::$invalid_file, 0000);
+        $logger = new TestLogger();
+        $cleaner = new LangFileCleaner(new Application($logger, $fake_sugar));
+        $this->assertTrue($cleaner->clean());
+        error_reporting($err_level);
     }
 }
